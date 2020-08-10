401469038b55699aa41e6df072df7b8fc0f1c990
WenchengZou <55119447+WenchengZou@users.noreply.github.com>
2020-08-10 15:56:57 +0800

    Update time_window.py

--- time_window.py
+++ time_window.py
@@ -16,24 +16,29 @@ df = pd.read_csv('us_daily_enhanced.csv')
 df = df.loc[0:127]
 #['','']
 df_ = {}
-def time_windows(window_length , data_name, df,alpha):
-    finall=[]
-    #this loop is for the entire dataframe:
-    #the last index for the window shall be the length of the entire dataframe minus the size of the windows plus 1
-    for index in range(len(df)-window_length+1):
-        #containing  each row for the finall result
-        temp=[]
-        #this loop is to calculate the result for the entire column_name
-        for index_dn in data_name:
+
+def time_windows(window_length, data_namex, data_namey, df, alpha):
+    finallx = []
+    finaly = []
+    # this loop is for the entire dataframe:
+    # the last index for the window shall be the length of the entire dataframe minus the size of the windows plus 1
+    for index in range(len(df) - window_length):
+        # containing  each row for the finall result
+        temp = []
+        # this loop is to calculate the result for the entire column_name
+        for index_dn in data_namex:
             sum = 0
-            #the result of each column shall be a number which we call sum
-            #and now we slide the window
+            # the result of each column shall be a number which we call sum
+            # and now we slide the window
             for windex in range(window_length):
-                sum+=(df[index_dn].values)[index+windex]*(alpha**windex)
+                sum += (df[index_dn].values)[index + windex] * (alpha ** windex)
             temp.append(sum)
-        finall.append(temp)
-    return finall
+        finallx.append(temp)
+
+    for indexy in range(window_length, len(df)):
+        finaly.append((df[data_namey].values)[indexy])
+    return np.array(finallx), np.array(finaly)
             
-print(time_windows(3,['positiveIncrease','hospitalizedIncrease'],df,0.8))
+print(time_windows(3,['positiveIncrease','hospitalizedIncrease'],['positiveIncrease'],df,0.8))
 
 
